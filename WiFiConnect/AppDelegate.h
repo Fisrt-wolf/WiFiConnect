20402ec07a176cee7a86ef28c774ce0512691dc3
IOS <ios@IOSdeMacBook-Air.local>
2014-06-06 16:56:05 +0800

    Initial Commit

---
+++ WiFiConnect/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WiFiConnect
+//
+//  Created by IOS on 14-6-6.
+//  Copyright (c) 2014年 IOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WiFiConnectTests/WiFiConnectTests.m
@@ -0,0 +1,34 @@
+//
+//  WiFiConnectTests.m
+//  WiFiConnectTests
+//
+//  Created by IOS on 14-6-6.
+//  Copyright (c) 2014年 IOS. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WiFiConnectTests : XCTestCase
+
+@end
+
+@implementation WiFiConnectTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
